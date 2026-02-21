901451996956b94aa24b89d3c3be4f49752d054a
tarun <119746382+tarun-1910@users.noreply.github.com>
2026-02-21 20:38:45 +0530

    Delete frontend directory

--- frontend/src/api/authApi.js
+++
@@ -1,8 +0,0 @@
-import api from "../api";
-
-
-const API = "/auth";
-
-export const registerUser = (data) => api.post("/auth/register", data);
-export const loginUser = (data) => api.post("/auth/login", data);
-
